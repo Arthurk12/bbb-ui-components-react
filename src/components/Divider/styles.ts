5683cf20aaf16535bef4ca497d9cf125097b883c
Arthurk12 <arthur_bockmann@hotmail.com>
2025-08-18 19:14:09 -0300

    fix: divider height

--- src/components/Divider/styles.ts
+++ src/components/Divider/styles.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components';
 import { colorBorderDefault } from '../../stylesheets/pallete';
 
 export const Divider = styled.hr`
-  min-height: 1px;
-  height: 1px;
-  background: ${colorBorderDefault}; 
   width: 100%;
+  border-top: 0px;
+  border-right: 0px;
+  border-left: 0px;
+  border-image: initial;
+  border-bottom: 1px solid ${colorBorderDefault};
+  margin: 0px;
 `;
